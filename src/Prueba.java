4c9f4a6ca3ac4b7224a37934ddca50342e94a76c
Alexander Silvera <116277740+AlexSil23@users.noreply.github.com>
2023-04-27 17:59:08 -0300

    Update Prueba.java

--- src/Prueba.java
+++ src/Prueba.java
@@ -1,13 +1,14 @@
 
 public class Prueba {
 	public static void main(String[] args) {
+		//Suma
 		Suma suma1=new Suma();
 		suma1.cargar1();
 		suma1.cargar2();
 		suma1.operar();
 		System.out.print("El resultado de la suma es: ");
 		suma1.mostrarResultado();
-		
+		//Resta
 		Resta resta1=new Resta();
 		resta1.cargar1();
 		resta1.cargar2();
